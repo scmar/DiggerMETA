1b28c3a48b05f703845943e42f7b2266ad33296a
scmar <Mario@scmar.de>
2018-09-07 07:12:39 +0200

    added french menu entries (thx jicehel)

--- DiggerMETA/Menu.cpp
+++ DiggerMETA/Menu.cpp
@@ -2,33 +2,41 @@
 const MultiLang lang_load[]= {
                { LANG_EN, "Load Game" },
                { LANG_DE, "Spiel laden"},
+               { LANG_FR, "Charger la partie"},
 };
 const MultiLang lang_save[]= {
                { LANG_EN, "Save Game" },
                { LANG_DE, "Spiel speichern"},
+               { LANG_FR, "Sauver la partie"},
 };
 const MultiLang lang_resume[]= {
                { LANG_EN, "Resume Game" },
                { LANG_DE, "Zurück"},
+               { LANG_FR, "Reprendre la partie"},
 };
 const MultiLang lang_new[]= {
                { LANG_EN, "New Game" },
                { LANG_DE, "Neues Spiel"},
+               { LANG_FR, "Nouvelle partie"},
 };
 const MultiLang lang_levelup[]= {
                { LANG_EN, "Level up" },
                { LANG_DE, "nächstes Level"},
+               { LANG_FR, "Niveau suivant"},
 };
 const MultiLang lang_leveldown[]= {
                { LANG_EN, "Level down" },
                { LANG_DE, "vorheriges Level"},
+               { LANG_FR, "Niveau précédent"},
 };
 const MultiLang lang_toggle[]= {
                { LANG_EN, "Toggle Mode" },
                { LANG_DE, "Modus wechseln"},
+               { LANG_FR, "Changer de mode"},
 };
 
 const MultiLang lang_title[]= {
 				{ LANG_EN, "Menu"},
 				{ LANG_DE,"Menü"},
+				{ LANG_FR,"Menu"},
 };
